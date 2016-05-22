3baf50db0de0a8acd2d011cf260ff0978cbba3fd
David <djl@colourcountry.net>
2016-05-22 18:37:00 +0100

    add left sense hat

--- display/device.py
+++ display/device.py
@@ -16,6 +16,7 @@ class Device:
         pass 
 
 class SenseHat(Device):
+    # this sense hat has the USB port on the right
     def __init__(self):
         if _SenseHat:
             self.hat = _SenseHat()
@@ -29,9 +30,16 @@ class SenseHat(Device):
         self.hat.clear()
 
 class VerticalSenseHat(SenseHat):
+    # this sense hat has the USB port on the top
     def __init__(self):
         SenseHat.__init__(self)
         self.hat.rotation = 90
+
+class LeftSenseHat(SenseHat):
+    # this sense hat has the USB port on the left
+    def __init__(self):
+        SenseHat.__init__(self)
+        self.hat.rotation = 180
 
 class UnicornHat(Device):
     def display(self, icon):
@@ -40,7 +48,7 @@ class UnicornHat(Device):
 CHOICES = { "stdout": Device,
             "astropi": SenseHat,
             "sense": SenseHat,
-            "v-astropi": VerticalSenseHat,
             "v-sense": VerticalSenseHat,
+            "l-sense": LeftSenseHat,
             "unicorn": UnicornHat }
 
--- sense_hat.sh
+++ sense_hat.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python3 display/main.py -l "$1" -d v-sense -c config.json
+python3 display/main.py -l "$1" -d l-sense -c config.json
 
 
